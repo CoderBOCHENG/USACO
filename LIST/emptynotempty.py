cd5a85530e2cc7d905dd47e2b932186fe6fb216d
BC_Coder <96875741+CoderBOCHENG@users.noreply.github.com>
2022-10-22 18:42:01 +0800

    Create emptynotempty.py

---
+++ LIST/emptynotempty.py
@@ -0,0 +1,44 @@
+list1 = []
+list2 = [5,10,15,20]
+list3 = ["tom", "ben", "tim", "bob"]
+if len(list1) == 0:
+    print("Empty")
+else:
+    print("Not Empty")
+
+if len(list2) == 0:
+    print("Empty")
+else:
+    print("Not Empty")
+
+
+if len(list3) == 0:
+    print("Empty")
+else:
+    print("Not Empty")
+
+
+
+if len(list3) == 0:
+    print("Empty List")
+else:
+    for i in range (len(list1)):
+        print(list1[i],i)
+
+
+
+
+if len(list2) == 0:
+    print("Empty List")
+else:
+    for i in range (len(list2)):
+        print(list2[i],i)
+
+if len(list3) == 0:
+    print("Empty List")
+else:
+    for i in range (len(list3)):
+        print(list3[i],i)
+
+
+
