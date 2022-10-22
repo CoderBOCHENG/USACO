102bcaa25c0900b31b81e923796f09c632bf26cb
BC_Coder <96875741+CoderBOCHENG@users.noreply.github.com>
2022-10-22 18:41:06 +0800

    Create counting.py

---
+++ LIST/counting.py
@@ -0,0 +1,20 @@
+list1 = [1,-2,-3,0,1,3,5,6]
+list2=[1,2,3,4,5,6,7,8,9,10]
+list3=[-9.-8.-7,-6,-5,-4,-2,-1]
+list4=[]
+
+
+count1 = 0
+for elem in list1:
+    if elem>4:
+        count1 += 1
+print(count1)
+
+
+
+
+count2 = 0
+for elem in list2:
+    if elem>4:
+        count2 += 1
+print(count2)
